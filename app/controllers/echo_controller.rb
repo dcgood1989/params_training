4256d193d6e1d9513809c4ee9bedadf53cc8c3cd
Dillon Good <dilloncgood@gmail.com>
2015-04-28 14:48:47 -0600

    params stuff completeD

--- app/controllers/echo_controller.rb
+++ app/controllers/echo_controller.rb
@@ -2,6 +2,9 @@ class EchoController < ApplicationController
 
   before_action do
     @params_to_print = {}
+# how to print params onto the screen to see what's goin on. The 40 stars breaks it up so you can see exactly where this is happening.
+    p "*"* 40
+    p params
   end
 
   def echo
